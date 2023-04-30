e063f27f1cae61dcbafa51c377bbe3ecda8ba704
Lee Yen Foong Ernest <leyefoer@gmail.com>
2023-04-30 15:10:57 +0800

    changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import Nav from 'react-bootstrap/Nav';
+
+export default function Navbar() {
+  return (
+    <Nav
+      activeKey="/home"
+    >
+      <Nav.Item>
+        <Nav.Link href="/home">Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
